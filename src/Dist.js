c53e4206272e36f9afd63d56e7701d79c4bb3270
Hjorthen <Hjorthensebastian@gmail.com>
2017-04-23 23:55:41 +0200

    Layout now loads from database

--- src/Dist.js
+++ src/Dist.js
@@ -1,21 +1,45 @@
-function login(){
+function loadTasks(){
 	var client = apigClientFactory.newClient();
-	var params = { taskID : '3'}
-	client.tasksTaskIDGet(params, {}).then(recieveData).catch(errorAccoured);
-	
+	var availableIds = ["1", "2", "3", "4"];
+	for(var id in availableIds){
+		var params = { "taskID" : availableIds[id]}
+		client.tasksTaskIDGet(params, {}).then(recieveData).catch(errorAccoured);
+	}
 }
 
-
 function errorAccoured(result){
-	//document.body.innerHTML = "<h1> Failed! </h1>";
-	print(result);
+}
+
+function print(token){
+	for(var t in token){
+		console.log("key: " + t + "value: " +token[t]);
+	}
 }
 
 
 function recieveData(result){
+	console.log(result.data.Task);
 	if(result.status == 200)
 	{
-		document.body.innerHTML = "<h1> Success! " +  JSON.stringify(result.data) + " </h1>";
+		if('content' in document.createElement('template'))
+		{
+			var template = document.querySelector('#taskentry');
+			var taskname = template.content.querySelector(".taskname");
+			var pricetag = template.content.querySelector(".pricetag");
+			var taskdesc = template.content.querySelector(".taskdescription");
+			taskname.textContent = result.data.Task.title;
+			pricetag.textContent = result.data.Task.price;
+			taskdesc.textContent = result.data.Task.description;
+			console.log(template);
+			var entry = document.importNode(template.content, true);
+			var list = document.querySelector('#taskcontainer');
+			list.appendChild(entry);	
+		}
+		else
+		{
+			window.alert("Please upgrade to a modern browser that supports <template> such as firefox or chrome");
+		}
 	}
+	
 	
 }
